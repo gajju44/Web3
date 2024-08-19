102cab0a8b1ec3ec6492bbd3696eb100d0b62d60
Gajendra Naphade <gajjunaphade@gmail.com>
2024-08-19 22:24:26 +0530

    assignment3

--- week1/assignment3.js
+++ week1/assignment3.js
@@ -5,8 +5,8 @@ function findHashWithPrefix(prefix) {
     let input = 0;
     while (true) {
         let inputStr = 
-"harkirat => Raman | Rs 100"
-"Ram => Ankit | Rs 10"
+`harkirat => Raman | Rs 100"
+Ram => Ankit | Rs 10`
  + input.toString();
         let hash = crypto.createHash('sha256').update(inputStr).digest('hex');
         if (hash.startsWith(prefix)) {
